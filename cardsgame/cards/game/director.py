811bf4829e5a59a025d1eeb3ca2a0d82f8feccc8
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-09-23 01:05:21 -0400

    update documentation

--- cardsgame/cards/game/director.py
+++ cardsgame/cards/game/director.py
@@ -7,9 +7,8 @@ class Director:
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
-        dice (List[Die]): A list of Die instances.
+        current_card (Instance of a class): An instance of Card class.
         is_playing (boolean): Whether or not the game is being played.
-        score (int): The score for one round of play.
         total_score (int): The score for the entire game.
     """
 
